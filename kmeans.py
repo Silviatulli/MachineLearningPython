84b513c75e6d5c485619b142580f4a1a2ed89830
Silvia <silvia.tulli.92@gmail.com>
2022-07-08 17:40:35 +0200

    kmeans

---
+++ kmeans.py
@@ -0,0 +1,38 @@
+
+# read university data
+import pandas as pd
+university=pd.read_csv('Universities.csv')
+university.head()
+
+# ignore the string column 
+x=university.iloc[:,1:]
+x.head()
+
+# Normalization 0-1
+from sklearn.preprocessing import MinMaxScaler
+norm=MinMaxScaler()
+for i in x.columns:
+    x[i]=norm.fit_transform(x[i].values.reshape(-1, 1))
+
+## K means clustering
+from sklearn.cluster import KMeans
+#Plot Elbow curve to get k value 
+#total within sum of squared
+twss=[]
+for i in range(2,10):
+    model=KMeans(n_clusters=i).fit(x)
+    twss.append(model.inertia_)
+
+import matplotlib.pyplot as plt
+plt.plot(range(2,10),twss,'-bo')
+plt.xlabel('Value of K')
+plt.ylabel('TWSS')
+plt.show()
+
+#If you know the k value
+from sklearn.cluster import KMeans
+model=KMeans(n_clusters=3).fit(x)
+model.labels_
+
+university['label']=model.labels_
+university.iloc[:,1:].groupby('label').mean()
