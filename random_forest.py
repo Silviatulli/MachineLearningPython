2baa51295d4bd98c69f4ff84f9df7afd714d37ab
Silvia <slvtulli@gmail.com>
2023-11-24 15:23:51 +0100

    Update random_forest.py

--- random_forest.py
+++ random_forest.py
@@ -1,18 +1,21 @@
-# read the data
+# Data Gathering
 from pydataset import data
 iris=data('iris')
 
-# split the data
 x=iris.iloc[:,:-1]
 y=iris.iloc[:,-1]
+
+# Train and Test Split
 from sklearn.model_selection import train_test_split
 trainx,testx,trainy,testy=train_test_split(x,y,test_size=.2)
 
-#Model creation
+# Model Creation
 from sklearn.ensemble import RandomForestClassifier
 model=RandomForestClassifier(n_estimators=10)
 model.fit(trainx,trainy)
 train_pred=model.predict(trainx)
+
+# Model Evaluation
 from sklearn.metrics import accuracy_score
 train_acc=accuracy_score(train_pred,trainy)
 test_pred=model.predict(testx)
@@ -20,10 +23,12 @@ test_acc=accuracy_score(test_pred,testy)
 print("Train acc",train_acc)
 print("Test_acc",test_acc)
 
-#create decision tree using information needed
+# Model Creation with different parameters
 model=RandomForestClassifier(n_estimators=100,criterion='entropy',n_jobs=2)
 model.fit(trainx,trainy)
 train_pred=model.predict(trainx)
+
+# Model Evaluation
 from sklearn.metrics import accuracy_score
 train_acc=accuracy_score(train_pred,trainy)
 test_pred=model.predict(testx)
