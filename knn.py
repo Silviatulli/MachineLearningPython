31fa6621dc07e046f1ba2acf7c63eb54129cbf83
Silvia <slvtulli@gmail.com>
2023-11-24 15:26:57 +0100

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,5 +1,5 @@
 ## KNN
-# Loading data set
+# Data Gathering
 from pydataset import data
 iris=data('iris')
 
@@ -7,16 +7,16 @@ iris=data('iris')
 x=iris.drop('Species',axis=1)
 y=iris['Species']
 
-#splitting the dataset
+# Train and Test Split
 from sklearn.model_selection import train_test_split
 trainx,testx,trainy,testy=train_test_split(x,y,test_size=.2)
 
-#KNN model creation
+# Model Creation
 from sklearn.neighbors import KNeighborsClassifier
 model=KNeighborsClassifier(n_neighbors=7)
 model.fit(trainx,trainy)
 
-#Evaluating
+# Model Evaluation
 train_pred=model.predict(trainx)
 test_pred=model.predict(testx)
 
@@ -27,18 +27,20 @@ test_acc=accuracy_score(test_pred,testy)
 print("Train Acc",train_acc)
 print("Test Acc",test_acc)
 
-#Finding K value
+# Finding K value
 acc=[]
 for i in range(1,25,2):
     model=KNeighborsClassifier(n_neighbors=i)
     model.fit(trainx,trainy)
     train_pred=model.predict(trainx)
     test_pred=model.predict(testx)
+    # Model Evaluation
     train_acc=accuracy_score(train_pred,trainy)
     test_acc=accuracy_score(test_pred,testy)
     acc.append([train_acc,test_acc,i])
     
 #matplotlib auto
+# Model Visualization
 import matplotlib.pyplot as plt
 plt.plot(range(1,25,2),[i[0] for i in acc],c='b')
 plt.plot(range(1,25,2),[i[1] for i in acc],c='r')
